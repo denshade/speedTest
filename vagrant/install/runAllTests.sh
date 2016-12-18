7bb22590371a6d228e749fc48b565fe6fa6799c2
denshade <lieven.vaneeckhaute@gmail.com>
2016-12-18 21:45:30 +0100

    Added lua

--- vagrant/install/runAllTests.sh
+++ vagrant/install/runAllTests.sh
@@ -95,6 +95,19 @@ do
 done
 
 #
+# lua
+#
+
+sudo apt-get install lua5.2
+cd /vagrant_data/lua
+for i in `seq 1 $NUMBEROFTESTS`
+do
+  /usr/bin/time --format "%e" -o /tmp/file lua primes.lua
+  cat /tmp/file >> ../reports/lua.numbers.csv
+done
+
+
+#
 # Pascal
 #
 echo "Pascal"
@@ -156,5 +169,6 @@ do
 done
 
 cd /vagrant_data/reports
+dos2unix *.csv
 echo *.csv | tr [:blank:] ','  > final/final.csv
 paste -d, *.csv >> final/final.csv
